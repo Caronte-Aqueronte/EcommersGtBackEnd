0b2ef9bcffb3eeeca6d339c2489b843d822416ca
Luis Monterroso <namenotfound4004@gmail.com>
2023-04-27 01:48:30 -0600

    Terminada la creacion y edicion de usuarios

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -3,7 +3,7 @@ const createHash = require('crypto');
 
 const login = async (req, res) => {
     //vemos si los parametros son nulos
-    if (req.body.correoElectronico == null || req.body.password == null || req.body.correoElectronico == "" ||req.body.password == "") {
+    if (req.body.correoElectronico == null || req.body.password == null || req.body.correoElectronico == "" || req.body.password == "") {
         res.json({
             respuesta: false//si fue mal entonces devolver false
         });
@@ -12,17 +12,35 @@ const login = async (req, res) => {
     req.body.password = createHash.createHash('sha256').update(req.body.password).digest('hex');//seteamos la contra como una encriptada
     //mandamos a buscar el usuario por su password y su usuario
     const usuarioEncontrado = await Usuario.findOne({ correoElectronico: req.body.correoElectronico, password: req.body.password });
-    
+
     if (usuarioEncontrado) {
-        res.json({ usuarioEncontrado});
-    }else{
+        res.json({ usuarioEncontrado });
+    } else {
+        res.send(null);
+    }
+}
+
+const traerUsuarioPorId = async (req, res) => {
+    //vemos si los parametros son nulos
+    if (req.query.id == null || req.query.id == "") {
+        res.json({
+            respuesta: false//si fue mal entonces devolver false
+        });
+        return;
+    }
+    //mandamos a buscar el usuario por su password y su usuario
+    const usuarioEncontrado = await Usuario.findById({ _id: req.query.id });
+
+    if (usuarioEncontrado) {
+        res.json({ usuarioEncontrado });
+    } else {
         res.send(null);
     }
 }
 
 const crearUsuario = async (req, res) => {
     //vemos si los parametros son nulos
-    if (req.body.correoElectronico == null || req.body.password == null || req.body.correoElectronico == "" ||req.body.password == "") {
+    if (req.body.correoElectronico == null || req.body.password == null || req.body.correoElectronico == "" || req.body.password == "") {
         res.json({
             respuesta: false//si fue mal entonces devolver false
         });
@@ -42,7 +60,69 @@ const crearUsuario = async (req, res) => {
     }
 }
 
+const crearUsuarioAdminOPaqueteria = async (req, res) => {
+    //vemos si los parametros son nulos
+    if (req.body.correoElectronico == null || req.body.password == null || req.body.correoElectronico == "" || req.body.password == "") {
+        res.json({
+            respuesta: false//si fue mal entonces devolver false
+        });
+        return;
+    }
+    req.body.password = createHash.createHash('sha256').update(req.body.password).digest('hex');//seteamos la contra como una encriptada
+    const insercion = await Usuario.insertMany(req.body);//mandamos a guardar al usuario en la db
+    if (insercion) {//si todo fue bien tonces revolvemos true
+        res.json({
+            respuesta: true
+        });
+    } else {
+        res.json({
+            respuesta: false//si fue mal entonces devolver false
+        });
+    }
+}
+
+const mostarUsuariosAdminYPqueteria = async (req, res) => {
+    const usuarios = await Usuario.find({ $or: [{ rol: { $eq: "Paqueteria" } }, { rol: { $eq: "Administrador" } }] });
+    res.json(usuarios);
+}
+
+
+const editarUsuario = async (req, res) => {
+    const nuevaInfo = req.body.nuevaInfo;
+    const id = req.body.id;
+    //vemos si los parametros son nulos
+    if (nuevaInfo.correoElectronico == null || nuevaInfo.password == null || nuevaInfo.correoElectronico == "" || nuevaInfo.password == ""
+        || nuevaInfo.rol == null || nuevaInfo.rol == "") {
+        res.json({
+            respuesta: false//si fue mal entonces devolver false
+        });
+        return;
+    }
+
+    nuevaInfo.password = createHash.createHash('sha256').update(nuevaInfo.password).digest('hex');//seteamos la contra como una encriptada
+    
+    const edicion = await Usuario.findOneAndUpdate({ _id: id }, {
+        correoElectronico: nuevaInfo.correoElectronico,
+        password: nuevaInfo.password, 
+        rol: nuevaInfo.rol
+    });//mandamos a guardar al usuario en la db
+
+    if (edicion) {//si todo fue bien tonces revolvemos true
+        res.json({
+            respuesta: true
+        });
+    } else {
+        res.json({
+            respuesta: false//si fue mal entonces devolver false
+        });
+    }
+}
+
 module.exports = {
     login: login,
-    crearUsuario: crearUsuario
+    crearUsuario: crearUsuario,
+    crearUsuarioAdminOPaqueteria: crearUsuarioAdminOPaqueteria,
+    mostarUsuariosAdminYPqueteria: mostarUsuariosAdminYPqueteria,
+    traerUsuarioPorId: traerUsuarioPorId,
+    editarUsuario:editarUsuario
 }
--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -5,7 +5,11 @@ const usuarioController = require("../controllers/UsuarioController");
 const router = express.Router();
 
 router.post('/login', usuarioController.login);//definiendo rutas
-
 router.post('/crearUsuario', usuarioController.crearUsuario);
+router.post('/crearUsuarioAdminOPaqueteria', usuarioController.crearUsuarioAdminOPaqueteria);
+router.post('/editarUsuario', usuarioController.editarUsuario);
+
+router.get('/mostarUsuariosAdminYPqueteria', usuarioController.mostarUsuariosAdminYPqueteria);
+router.get('/traerUsuarioPorId', usuarioController.traerUsuarioPorId);
 
 module.exports = router;//exporar el routers
